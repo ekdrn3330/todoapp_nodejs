57203d3e5af3a2197d9fa8aa01159455af136f2a
KimJinHyeok <ekdrn3330@naver.com>
2021-09-05 02:07:26 +0900

    210905nodejs어렵...

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+
+const http = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
 app.use(express.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override');
@@ -16,7 +21,7 @@ MongoClient.connect(process.env.DB_URL, (err, client)=>{
 
     db = client.db('todoapp');
 
-    app.listen(process.env.PORT, ()=>{
+    http.listen(process.env.PORT, ()=>{
         console.log('listening on 8080\nhttp://localhost:8080');
     });
 
@@ -76,9 +81,78 @@ app.put('/edit', (req, res)=>{
     });
 });
 
-app.get('/fail', (req, res)=>{
-    res.send('로그인에 실패했습니다.');
+app.get('/login_fail', (req, res)=>{
+    res.render('login_fail.ejs');
 });
+
+app.get('/sign_up', (req, res)=>{
+    res.render('register.ejs');
+});
+
+let multer = require('multer');
+var storage = multer.diskStorage({
+    destination : function(req, file, cb){
+        cb(null, './public/image');
+    },
+    filename : function(req, file, cb){
+        cb(null, file.originalname);
+    },
+    fileFilter: function (req, file, callback) { //파일 확장자 제한
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+            return callback(new Error('PNG, JPG만 업로드하세요'));
+        }
+        callback(null, true);
+    },
+    limits:{ //파일크기 제한
+        fileSize: 1024 * 1024
+    }
+});
+
+var upload = multer({storage : storage});
+
+app.get('/upload', (req, res)=>{
+    res.render('upload.ejs');
+});
+
+app.post('/upload', upload.array('profile', 10), (req, res)=>{
+    res.send('업로드완료');
+});
+
+app.get('/image/:imageName', (req, res)=>{
+    res.sendFile(__dirname + '/public/image/' + req.params.imageName);
+});
+
+app.get('/chat', (req, res)=>{
+    res.render('chat.ejs');
+});
+
+io.on('connection', function(socket){
+    console.log('연결되었어요.');
+
+    socket.on('인삿말', function(data){
+        console.log('인사말이수신되었음',data);
+        io.emit('퍼트리기', data);
+    });
+});
+
+var chat1 = io.of('/채팅방1');
+chat1.on('connection', function(socket){
+    console.log('채팅방1에 연결되었어요.');
+
+    var 방번호 = '';
+    socket.on('방들어가고픔', function(data){
+        socket.join(data);
+        방번호 = data;
+    });
+
+    socket.on('인삿말', function(data){
+        console.log('인사말이수신되었음',data);
+        chat1.to(방번호).emit('퍼트리기', data);
+    });
+});
+
+
 
 
 
@@ -99,7 +173,7 @@ app.get('/login', (req, res)=>{
     res.render('login.ejs');
 });
 app.post('/login', passport.authenticate('local', {
-    failureRedirect : '/fail'
+    failureRedirect : '/login_fail'
 }), (req, res)=>{
     req.session.save(function(){
         res.redirect('/');
@@ -115,7 +189,7 @@ function loginChk(req, res, next){
     if(req.user){
         next();
     } else {
-        res.send('로그인 안 하셨는데요?');
+        res.render('login_chk.ejs');
     }
 }
 
@@ -134,13 +208,13 @@ passport.use(new LocalStrategy({
   }, function (입력한아이디, 입력한비번, done) {
     //console.log(입력한아이디, 입력한비번);
     db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
-      if (에러) return done(에러)
+      if (에러) return done(에러);
   
-      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' });
       if (입력한비번 == 결과.pw) {
-        return done(null, 결과)
+        return done(null, 결과);
       } else {
-        return done(null, false, { message: '비번틀렸어요' })
+        return done(null, false, { message: '비번틀렸어요' });
       }
     })
 }));
@@ -160,7 +234,7 @@ app.post('/register', (req, res)=>{
     });
 });
 
-app.post('/add', (req,res)=>{
+app.post('/add', loginChk, (req,res)=>{
     db.collection('counter').findOne({name : '게시물갯수'}, (err, result)=>{
         console.log(result.totalPost);
         var totalPost = result.totalPost;
@@ -183,3 +257,6 @@ app.delete('/delete', (req, res)=>{
         res.status(200).send({message : '성공했습니다'});
     });
 });
+
+app.use('/shop', require('./routes/shop.js'));
+app.use('/board/sub', require('./routes/board.js'));
